11d07290d213c734da7d5042388c66de010b0093
Vikram Jain <jainvikram444@gmail.com>
2023-02-24 02:48:01 +0530

    Remove unwanted file

--- app.js
+++ app.js
@@ -3,10 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-//var productsRouter = require('./routes/product.routes');
+// const swaggerUi = require('swagger-ui-express');
+// const swaggerDocument = require('./swagger.json');
 
 var app = express();
 
@@ -20,10 +18,10 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-//app.use('/products', productsRouter);
+
 require("./routes/product.routes.js")(app);
+// router.use('/api-docs', swaggerUi.serve);
+// router.get('/api-docs', swaggerUi.setup(swaggerDocument));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
