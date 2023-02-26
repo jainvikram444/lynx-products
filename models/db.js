a8505157b2f43d4879fc882b258d868e1e853d3e
Vikram Jain <jainvikram444@gmail.com>
2023-02-27 03:51:38 +0530

    Add enviorment config and migration

--- models/db.js
+++ models/db.js
@@ -1,18 +1,19 @@
 const mysql = require("mysql");
-const config = require('config');
+const config = require('../config/database.json');
+const node_env = process.env.NODE_ENV
 
 // Create a connection to the database
-const connection = mysql.createConnection({
-  host: config.get("DB.HOST"),
-  user: config.get("DB.USER"),
-  password: config.get("DB.PASSWORD"),
-  port: config.get("DB.PORT"),
-  database: config.get("DB.DATABASE")
+const connection = mysql.createConnection({  
+  host: config[node_env]["host"],
+  user: config[node_env]["user"],
+  password: config[node_env]["password"],
+  port: config[node_env]["port"],
+  database: config[node_env]["database"]
 });
 
 // open the MySQL connection
 connection.connect(error => {
-  console.log(config.get("DB.PORT"));
+  console.log(node_env, config[node_env]);
   if (error) throw error; 
   console.log("Successfully connected to the database.");
 });
